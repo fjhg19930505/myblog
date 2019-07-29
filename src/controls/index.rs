400a003ae68e95138e9a3e01bf6cac1d6a995e4b
unknown <ziyoulu1993@163.com>
2019-07-29 19:58:38 +0800

    前端代码引入

--- src/controls/index.rs
+++ src/controls/index.rs
@@ -1,5 +1,12 @@
-use actix_web::{web, App, HttpResponse, HttpServer, Responder};
+use actix_web::{HttpRequest, HttpResponse};
+use futures::stream::once;
+use bytes::Bytes;
+use actix_http::body::Body;
 
-pub fn go() -> impl Responder {
-    HttpResponse::Ok().body("Hello world!")
+pub fn go(req: &HttpRequest) -> HttpResponse {
+    let body = once(Ok(Bytes::from_static(b"views/index")));
+
+    HttpResponse::Ok()
+        .content_type("application/json")
+        .body(Body::Streaming(Box::new(body)))
 }
--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,39 @@
-use actix_web::{web, App, HttpResponse, HttpServer, Responder};
+#[macro_use]
+extern crate actix_web;
 
-mod controls;
+#[macro_use]
+extern crate serde_json;
 
-fn main() {
-    HttpServer::new(|| {
+use actix_web::web;
+use actix_web::{App, HttpResponse, HttpServer};
+
+use handlebars::Handlebars;
+
+use std::io;
+
+#[get("/")]
+fn index(hb: web::Data<Handlebars>) -> HttpResponse {
+    let data = json!({
+
+    });
+    let body = hb.render("index", &data).unwrap();
+    println!("{:?}", body);
+
+    HttpResponse::Ok().body(body)
+}
+
+fn main() -> io::Result<()> {
+    let mut handlebars = Handlebars::new();
+    handlebars
+        .register_templates_directory(".html", "./static/views")
+        .unwrap();
+    let handlebars_ref = web::Data::new(handlebars);
+
+    HttpServer::new(move || {
         App::new()
-            .route("/", web::get().to(controls::index::go))
-            .route("/again", web::get().to(controls::index2::go))
+            .register_data(handlebars_ref.clone())
+            .service(index)
     })
-    .bind("127.0.0.1:8080")
-    .unwrap()
+    .bind("192.168.1.39:8080")?
     .run()
-    .unwrap();
 }
