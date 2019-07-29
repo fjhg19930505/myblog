e19b3bab4ec56ebbf7d56316e664a421fe21b5fb
unknown <ziyoulu1993@163.com>
2019-07-29 22:03:22 +0800

    404界面

--- src/main.rs
+++ src/main.rs
@@ -1,39 +1,93 @@
 #[macro_use]
 extern crate actix_web;
 
-#[macro_use]
-extern crate serde_json;
+use std::{env, io};
 
-use actix_web::web;
-use actix_web::{App, HttpResponse, HttpServer};
-
-use handlebars::Handlebars;
-
-use std::io;
+use actix_files as fs;
+use actix_session::{CookieSession, Session};
+use actix_web::http::{header, Method, StatusCode};
+use actix_web::{
+    error, guard, middleware, web, App, Error, HttpRequest, HttpResponse, HttpServer,
+    Result,
+};
+use bytes::Bytes;
+use futures::unsync::mpsc;
+use futures::{future::ok, Future, Stream};
 
 #[get("/")]
-fn index(hb: web::Data<Handlebars>) -> HttpResponse {
-    let data = json!({
+fn index(req: HttpRequest) -> impl Responder {
+    Ok(HttpResponse::build(StatusCode::OK)
+        .content_type("text/html; charset=utf-8")
+        .body(include_str!("../static/index.html")))
+}
 
-    });
-    let body = hb.render("index", &data).unwrap();
-    println!("{:?}", body);
-
-    HttpResponse::Ok().body(body)
+/// 404 handler
+fn p404() -> Result<fs::NamedFile> {
+    Ok(fs::NamedFile::open("static/404.html")?.set_status_code(StatusCode::NOT_FOUND))
 }
 
 fn main() -> io::Result<()> {
-    let mut handlebars = Handlebars::new();
-    handlebars
-        .register_templates_directory(".html", "./static/views")
-        .unwrap();
-    let handlebars_ref = web::Data::new(handlebars);
+    env::set_var("RUST_LOG", "actix_web=debug");
+    env_logger::init();
+    let sys = actix_rt::System::new("basic-example");
 
-    HttpServer::new(move || {
+    HttpServer::new(|| {
         App::new()
-            .register_data(handlebars_ref.clone())
+            // cookie session middleware
+            .wrap(CookieSession::signed(&[0; 32]).secure(false))
+            // enable logger - always register actix-web Logger middleware last
+            .wrap(middleware::Logger::default())
+            // register simple route, handle all methods
             .service(index)
+            // with path parameters
+            /*.service(web::resource("/user/{name}").route(web::get().to(with_param)))
+            // async handler
+            .service(
+                web::resource("/async/{name}").route(web::get().to_async(index_async)),
+            )
+            // async handler
+            .service(
+                web::resource("/async-body/{name}")
+                    .route(web::get().to(index_async_body)),
+            )
+            .service(
+                web::resource("/test").to(|req: HttpRequest| match *req.method() {
+                    Method::GET => HttpResponse::Ok(),
+                    Method::POST => HttpResponse::MethodNotAllowed(),
+                    _ => HttpResponse::NotFound(),
+                }),
+            )
+            .service(web::resource("/error").to(|| {
+                error::InternalError::new(
+                    io::Error::new(io::ErrorKind::Other, "test"),
+                    StatusCode::INTERNAL_SERVER_ERROR,
+                )
+            }))
+            // static files
+            .service(fs::Files::new("/static", "static").show_files_listing())
+            // redirect
+            .service(web::resource("/").route(web::get().to(|req: HttpRequest| {
+                println!("{:?}", req);
+                HttpResponse::Found()
+                    .header(header::LOCATION, "static/welcome.html")
+                    .finish()
+            })))*/
+            // default
+            .default_service(
+                // 404 for GET request
+                web::resource("")
+                    .route(web::get().to(p404))
+                    // all requests that are not `GET`
+                    .route(
+                        web::route()
+                            .guard(guard::Not(guard::Get()))
+                            .to(|| HttpResponse::MethodNotAllowed()),
+                    ),
+            )
     })
-    .bind("192.168.1.39:8080")?
-    .run()
+    .bind("127.0.0.1:8080")?
+    .start();
+
+    println!("Starting http server: 127.0.0.1:8080");
+    sys.run()
 }
