3f3738c2b115fa329a93b583ed7b95d09b153bf9
unknown <ziyoulu1993@163.com>
2019-07-30 20:12:23 +0800

    解析static资源

--- src/main.rs
+++ src/main.rs
@@ -14,22 +14,49 @@ use bytes::Bytes;
 use futures::unsync::mpsc;
 use futures::{future::ok, Future, Stream};
 
+/// favicon handler
+fn js(req: HttpRequest, path: web::Path<(String, String)>) -> Result<fs::NamedFile> {
+    if path.0 != "" {
+        if path.1 != "" {
+            return Ok(fs::NamedFile::open(format!("static/js/{}/{}", path.0, path.1))?);
+        }
+    }
+    return Ok(fs::NamedFile::open(format!("static/js/{}", path.0))?);
+}
+
+fn img(req: HttpRequest, path: web::Path<(String, String)>) -> Result<fs::NamedFile> {
+    if path.0 != "" {
+        if path.1 != "" {
+            return Ok(fs::NamedFile::open(format!("static/img/{}/{}", path.0, path.1))?);
+        }
+    }
+    return Ok(fs::NamedFile::open(format!("static/img/{}", path.0))?);
+}
+
+fn css(req: HttpRequest, path: web::Path<(String,)>) -> Result<fs::NamedFile> {
+    Ok(fs::NamedFile::open(format!("static/css/{}", path.0))?)
+}
+
+fn fonts(req: HttpRequest, path: web::Path<(String,)>) -> Result<fs::NamedFile> {
+    Ok(fs::NamedFile::open(format!("static/fonts/{}", path.0))?)
+}
+
 #[get("/")]
-fn index(req: HttpRequest) -> impl Responder {
+fn index(req: HttpRequest) -> Result<HttpResponse> {
     Ok(HttpResponse::build(StatusCode::OK)
         .content_type("text/html; charset=utf-8")
-        .body(include_str!("../static/index.html")))
+        .body(include_str!("../static/views/index.html")))
 }
 
 /// 404 handler
 fn p404() -> Result<fs::NamedFile> {
-    Ok(fs::NamedFile::open("static/404.html")?.set_status_code(StatusCode::NOT_FOUND))
+    Ok(fs::NamedFile::open("static/views/404.html")?.set_status_code(StatusCode::NOT_FOUND))
 }
 
 fn main() -> io::Result<()> {
     env::set_var("RUST_LOG", "actix_web=debug");
     env_logger::init();
-    let sys = actix_rt::System::new("basic-example");
+    let sys = actix_rt::System::new("myblog");
 
     HttpServer::new(|| {
         App::new()
@@ -40,38 +67,10 @@ fn main() -> io::Result<()> {
             // register simple route, handle all methods
             .service(index)
             // with path parameters
-            /*.service(web::resource("/user/{name}").route(web::get().to(with_param)))
-            // async handler
-            .service(
-                web::resource("/async/{name}").route(web::get().to_async(index_async)),
-            )
-            // async handler
-            .service(
-                web::resource("/async-body/{name}")
-                    .route(web::get().to(index_async_body)),
-            )
-            .service(
-                web::resource("/test").to(|req: HttpRequest| match *req.method() {
-                    Method::GET => HttpResponse::Ok(),
-                    Method::POST => HttpResponse::MethodNotAllowed(),
-                    _ => HttpResponse::NotFound(),
-                }),
-            )
-            .service(web::resource("/error").to(|| {
-                error::InternalError::new(
-                    io::Error::new(io::ErrorKind::Other, "test"),
-                    StatusCode::INTERNAL_SERVER_ERROR,
-                )
-            }))
-            // static files
-            .service(fs::Files::new("/static", "static").show_files_listing())
-            // redirect
-            .service(web::resource("/").route(web::get().to(|req: HttpRequest| {
-                println!("{:?}", req);
-                HttpResponse::Found()
-                    .header(header::LOCATION, "static/welcome.html")
-                    .finish()
-            })))*/
+            .service(web::resource("/js/{name}/{name2}").route(web::get().to(js)))
+            .service(web::resource("/img/{name}/{name2}").route(web::get().to(img)))
+            .service(web::resource("/css/{name}").route(web::get().to(css)))
+            .service(web::resource("/fonts/{name}").route(web::get().to(fonts)))
             // default
             .default_service(
                 // 404 for GET request
@@ -88,6 +87,6 @@ fn main() -> io::Result<()> {
     .bind("127.0.0.1:8080")?
     .start();
 
-    println!("Starting http server: 127.0.0.1:8080");
+    println!("Starting http port :8080");
     sys.run()
 }
